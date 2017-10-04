d95eb022b695c6d78055f3edd0cb34cab646425b
11eighty <tjboyd20@gmail.com>
2017-10-04 17:16:17 -0500

    initial commit

---
+++ client/main.js
@@ -0,0 +1,26 @@
+import { Meteor } from 'meteor/meteor';
+import ReactDOM from 'react-dom';
+import { Tracker } from 'meteor/tracker';
+import { Session } from 'meteor/session';
+import { browserHistory } from 'react-router';
+
+import { routes, onAuthChange } from './../imports/routes/routes';
+import './../imports/startup/simple-schema-configuration.js';
+
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId();
+  onAuthChange(isAuthenticated);
+});
+
+Tracker.autorun(() => {
+  const selectedNotedId = Session.get('selectedNoteId');
+
+  if (selectedNotedId) {
+    browserHistory.replace(`/dashboard/${selectedNotedId}`);
+  }
+});
+
+Meteor.startup(() => {
+  Session.set('selectedNoteId', undefined);
+  ReactDOM.render(routes, document.getElementById('app'));
+});
---
+++ imports/ui/NoteListItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import moment from 'moment';
+import { Session } from 'meteor/session';
+import { createContainer } from 'meteor/react-meteor-data';
+
+import PropTypes from 'prop-types';
+
+export const NoteListItem = (props) => {
+  return (
+    <div onClick={() => {
+      props.Session.set('selectedNoteId', props.note._id);
+    }}>
+      <h5>{ props.note.title || 'Untitled note' }</h5>
+      <p>{ moment(props.note.updatedAt).format('M/DD/YY') }</p>
+    </div>
+  );
+};
+
+NoteListItem.propTypes = {
+  note: PropTypes.object.isRequired,
+  Session: PropTypes.object.isRequired
+};
+
+export default createContainer(() => {
+  return { Session };
+}, NoteListItem);
